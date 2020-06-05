548b8855f7623d8ae1fb6dcd4ae4ade49f0be95a
Alper Berber <berber@sabanciuniv.edu>
2020-06-05 04:51:47 +0300

    Update in wizard

--- wizard.js
+++ wizard.js
@@ -5,18 +5,16 @@
 const fs = require('fs')
 
 var data;
-fs.readFile('data.json', (err, content) => {
-    if (err) throw err
-    data = JSON.parse(content)
-    var SUWizard = new Wizard(data, ["CS 201", "SPS 102", "MATH 102", "NS 102", "TLL 102"], []);
-})
+data = JSON.parse(fs.readFileSync('data.json'))
+
+
 class Wizard {
     /**
      * Constructs class' basic properties.
-     * @param  {Object} datasheet File where all the courses are stored
-     * @param  {[string]} coursesWanted Courses wanted, notated as shortcode (e.g CS 201)
-     * @param {[Object]} options Options specified for the algorithm
-     * @returns {[any]}
+     * @param  {{}} datasheet File where all the courses are stored
+     * @param  {[string]} coursesWanted Courses wanted, notated in shortcode (e.g CS 201)
+     * @param {[{}]} options Options specified for the algorithm
+     * @returns {[{}]}
      */
     constructor(datasheet, courses, options){
         this.data = datasheet.courses
@@ -29,7 +27,13 @@ class Wizard {
         }
         this.sortCourses()
         this.getSections()
+        this.prioritizeCourses()
+        this.createSchedule()
+        this.rearrangeSchedule()
     }
+    /**
+     * Modifies the structure of the course objects to a more useful structure.
+     */
     sortCourses(){
         let newCourses = [];
         for(let i = 0; i < this.data.length; i++){}
@@ -74,21 +78,148 @@ class Wizard {
             }
         })
     }
+    /**
+     * Prioritizes courses primarily looking at its lecture count, alternatively its recit count
+     */
     prioritizeCourses(){
-        this.courses.forEach(course => {
+        var count = []
+        this.courses.forEach((course, index) => {
             course.sectionCount.forEach(day => {
                 course.lectureCount += day[0]
                 course.recitCount += day[1]
             })
+            count.push([index, course.lectureCount, course.recitCount])
         })
+        count.sort((a, b) => {
+            if(a[1] > b[1]) return 1;
+            else if(a[1] < b[1]) return -1;
+            else{
+                if(a[2] > b[2]) return 1;
+                else if(a[2] < b[2]) return -1;
+                else return 1;
+            }
+        })
+        var modifiedCourses = []
+        count.forEach(el => {
+            modifiedCourses.push(this.courses[el[0]]);
+        })
+        this.courses = modifiedCourses
+    }
+    /**
+     * Checks if specified section has a time conflict with any other section already inserted.
+     * @param  {{}} section
+     * @param  {[]} schedule
+     */
+    isConflict(section, schedule){
+        var check = false;
+        if(schedule.length == 0) return false;
+        //TODO try to reduce the time it takes, its n^4!!!!
+        for(let i = 0; i < schedule.length; i++){
+            for(let k = 0; k < section.schedule.length; k++){
+                for(let n = 0; n < schedule[i].sections.length; n++){
+                    for(let m = 0; m < schedule[i].sections[n].schedule.length; m++){
+                        var cSection = schedule[i].sections[n].schedule[m];
+                        var sectionClass = section.schedule[k];
+                        if(
+                            (cSection.day == sectionClass.day) && 
+                            (
+                                (
+                                    (sectionClass.start >= cSection.start) && 
+                                    (sectionClass.start < (cSection.start + cSection.duration))
+                                ) ||
+                                (
+                                    (sectionClass.start + sectionClass.duration > cSection.start) && 
+                                    (sectionClass.start + sectionClass.duration <= cSection.start + cSection.duration)
+                                )
+                            )
+                        ) {
+                            check = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return check;
+    }
+    randomInteger(min, max){
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+    createSchedule(){
+        var testSchedule = []
 
 
+        //TODO this function becomes n^8!!!!!!!!!!
+        for(var i = 0; i < this.courses.length; i++){
+            var course = this.courses[i];
+            var possibleSections = []
+            var possibleRSections = []
+            
+            for(var k = 0; k < course.lectures.sections.length; k++){
+                var section = course.lectures.sections[k];
+                if(!this.isConflict(section, testSchedule)){
+                    possibleSections.push(section);
+                }
+            }
+            if(course.recitations != null){
+                for(var l = 0; l < course.recitations.sections.length; l++){
+                    var rsection = course.recitations.sections[l];
+                    if(!this.isConflict(rsection, testSchedule)){
+                        possibleRSections.push(rsection);
+                    }
+                }
+            }
+            
+            if(possibleSections.length == 0){
+                console.log("No available sections for " + course.name);
+                i = 0;
+                k = 0;
+                possibleSections = []
+                possibleRSections = []
+                continue;
+            }
+            else{
+                var tempCourse = {
+                    name: course.name,
+                    code: course.code,
+                    sections: [possibleSections[this.randomInteger(0, possibleSections.length-1)], possibleRSections[this.randomInteger(0, possibleRSections.length-1)]],
+                }
+                do {
+                    if((tempCourse.code == "SPS 101" || tempCourse.code == "SPS 102") && tempCourse.sections[0].schedule[0].day != tempCourse.sections[1].schedule[0].day){
+                        tempCourse.sections = [possibleSections[this.randomInteger(0, possibleSections.length-1)], possibleRSections[this.randomInteger(0, possibleRSections.length-1)]]
+                        console.log(tempCourse.sections[0].schedule[0].day != tempCourse.sections[1].schedule[0].day);
+                    }
+                }
+                while((tempCourse.code == "SPS 101" || tempCourse.code == "SPS 102") && tempCourse.sections[0].schedule[0].day != tempCourse.sections[1].schedule[0].day){
+                    tempCourse.sections = [possibleSections[this.randomInteger(0, possibleSections.length-1)], possibleRSections[this.randomInteger(0, possibleRSections.length-1)]]
+                }
+                testSchedule.push(tempCourse)
+            }
+        }
+        this.testSchedule = testSchedule;
+    }
+    rearrangeSchedule(){
+        for(var i = 0; i < this.testSchedule.length; i++){
+            for(var k = 0; k < this.testSchedule[i].sections.length; k++){
+                if(this.testSchedule[i].sections[k] == undefined) continue;
+                for(var n = 0; n < this.testSchedule[i].sections[k].schedule.length; n++){
+                    this.schedule[this.testSchedule[i].sections[k].schedule[n].day].push(this.testSchedule[i])
+                }
+               
+            }
+        }
     }
 
 
 }
+var SUWizard = new Wizard(data, ["CS 201", "SPS 102", "MATH 102", "NS 102", "TLL 102"], []);
+SUWizard.schedule.forEach(day => {
+    day.forEach(course => {
+        course.sections.forEach(sec => {
+            console.log(sec);
+        })
+    })
+})
 
-module.exports = {
-    Wizard: Wizard
-};
+module.exports = {Wizard};
 
--- wizard.test.js
+++ wizard.test.js
@@ -28,4 +28,31 @@ describe('Wizard class fn test', () => {
             })
         })
     })
+
+    context('test for time conflict function', () => {
+        it('should find whether there\'s a conflict or not', () => {
+            var time_table = [
+                {
+                    // Course here
+                    sections: [
+                        // Lecture and recit sections here
+                        {
+                            // Details about the section here
+                            "schedule": [{ "day": 1, "start": 4, "duration": 2 }],
+                        }
+                    ]
+                }
+            ]
+            var testSection = {
+                "schedule": [
+                    { "day": 1, "start": 5, "duration": 1 }
+                ],
+            }
+            var textSection2 = {
+                "schedule": [{ "day": 3, "start": 4, "duration": 2 }],
+            }
+            expect(SUWizard.isConflict(testSection, time_table)).to.be.equal(true);
+            expect(SUWizard.isConflict(textSection2, time_table)).to.be.equal(false);
+        })
+    })
 })
